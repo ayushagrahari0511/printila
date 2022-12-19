53e9857afcededdf6178da056ae4e1918705337c
ayushagrahari0511 <ayushagrahari0511@protonmail.com>
2022-12-19 19:59:19 +0530

    Initial commit

--- components/Login/Phone/Phone.jsx
+++ components/Login/Phone/Phone.jsx
@@ -1,9 +1,24 @@
 import styles from './Phone.module.css'
+import { useState } from 'react'
 import printila from '../../../public/printila_logo.png'
 import Image from 'next/image'
 
-
 const Phone = () => {
+    const [phone, setPhone] = useState()
+    const [checked, setChecked] = useState(false)
+
+    // Function to store the number in state
+    const inputPhone = (e) => {
+        setPhone(e.target.value)
+    }
+
+    const inputChecked = () => {
+        setChecked(!checked)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
@@ -12,16 +27,16 @@ const Phone = () => {
                 <form className={styles.form}>
                     <div className={styles.phone}>
                         <label htmlFor="phone">Mobile number</label>
-                        <input type="text" inputMode='numeric' name="phone" id="phone" maxLength={10} min={10} placeholder='Enter Your phone no.' />
+                        <input type="text" inputMode='numeric' onChange={inputPhone} value={phone} name="phone" id="phone" maxLength={10} min={10} max={10} placeholder='Enter Your phone no.' />
                     </div>
                     <div className={styles.checkbox}>
-                        <input type="checkbox" required name='terms&condition' id='terms&condition' />
-                        <label htmlFor="terms&condition">
+                        <input type="checkbox" name='terms&condition' id='terms&condition' onChange={inputChecked} />
+                        <label htmlFor="terms&condition" onChange={inputChecked}>
                             By clicking you are agreeing to our <span style={{ color: "var(--dark_red)" }}> Terms & Condition </span>
                             and Privacy Policy
                         </label>
                     </div>
-                    <button>
+                    <button onClick={handleSubmit} disabled={!checked || !phone}>
                         Get Started
                     </button>
                 </form>
